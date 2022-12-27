629e3af81962f78a1b07901feaa001f43e8d02bb
Olanrewaju Bayode <lanrebayode77@gmail.com>
2022-12-27 16:58:51 +0100

    first commit

---
+++ javascript/script.js
@@ -0,0 +1,7 @@
+/* typing Animation */
+var typed = new Typed('.typing',{
+    strings:['','Blockchain Developer','Web Developer','Smart Contract','Digital Expert','DeFi Technology'],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
